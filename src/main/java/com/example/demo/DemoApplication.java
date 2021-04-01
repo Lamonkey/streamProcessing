ae585669988723960522446ea9a57bc6db9f45b6
lamonkey <88therisingsun@gmail.com>
2021-04-01 07:20:11 -0400

    changed home directory

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,22 +1,21 @@
 package com.example.demo;
 
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.stereotype.*;
 
+@Controller
 @SpringBootApplication
-@RestController
 public class DemoApplication {
 
-public static void main(String[] args) {
-SpringApplication.run(DemoApplication.class, args);
-}
+    @RequestMapping("/")
+    @ResponseBody
+    String home() {
+      return "Hello World!";
+    }
 
-@GetMapping("/main")
-public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
-return String.format("Hello %s!", name);
-}
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
 }
