39efac9d1e5c94f178f1571348f2bda7a29f245e
lamonkey <88therisingsun@gmail.com>
2021-04-05 16:42:41 -0400

    added model

--- spConverter/settings.py
+++ spConverter/settings.py
@@ -12,7 +12,8 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 import os
 from pathlib import Path
-
+#import psycopg2
+import django_heroku 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -42,6 +43,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -126,7 +128,14 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
-# STATICFILES_DIRS = (
-#     os.path.join(BASE_DIR, 'static'),
-# )
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+STATICFILES_DIRS = (
+    os.path.join(PROJECT_ROOT, 'static'),
+)
+
+django_heroku.settings(locals())
+# Simplified static file serving.
+# https://warehouse.python.org/project/whitenoise/
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
--- streamData/views.py
+++ streamData/views.py
@@ -2,11 +2,34 @@ from typing import Reversible
 from django.http.response import HttpResponseRedirect
 from django.shortcuts import render
 from django.http import HttpResponse
-from django import forms
+from django.forms import ModelForm
 from django.urls import reverse
+from .models import Flight, Ticket
+from django import forms
 # Create your views here.
+# class NewDataForm(ModelForm):
+#     class Meta:
+#         model = Flight
+#         fields = ['origin','destination','duration']
+
+class multiTickets(forms.Field):
+    def to_python(self,value):
+        if not value:
+            return[]
+        return value.split('\n')
+    def validate(self,value):
+        super().validate(value)
+        for flight in value:
+            if len(flight) != 3:
+                return False
+
 class NewDataForm(forms.Form):
-    data = forms.CharField(label="New Data")
+    data = multiTickets(label="data",widget=forms.Textarea)
+   
+
+
+    
+
 
 def index(request):
     return render(request,'streamData/index.html')
@@ -15,18 +38,30 @@ def streamData(request,rate):
     return render(request,'streamData/template.html',{
         "rate":rate
     })
-data = []
+
 def displayData(request):
+    start = int(request.GET.get("start") or 0)
+    end = int(request.GET.get('end') or (start + 9))
     return render(request,"streamData/displayData.html",{
-        "data":data
+        "data":Ticket.objects.all()[start:end]
     })
 
 def addData(request):
     if request.method == "POST":
         form = NewDataForm(request.POST)
+        #print(request.POST)
         if form.is_valid():
             newData = form.cleaned_data["data"]
-            data.append(newData)
+            for line in newData:
+                p = line.split(',')
+                created = Ticket(
+                    ticket_no=p[0],
+                    flight_id=p[1],
+                    fare_conditions=p[2],
+                    amount = p[3]
+                )
+                created.save()
+                
             return HttpResponseRedirect(reverse("streamData:displayData"))
         else:
             return render(request,"streamData/addData.html",{
