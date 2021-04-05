453e9510c534df9e08bc90850b220f03837c3f8a
lamonkey <88therisingsun@gmail.com>
2021-04-04 20:46:31 -0400

    added runtime

--- spConverter/settings.py
+++ spConverter/settings.py
@@ -119,10 +119,4 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 STATIC_URL = '/static/'
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-
-STATIC_ROOT = os.path.normpath(os.path.join(BASE_DIR, 'staticfiles'))
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
 
