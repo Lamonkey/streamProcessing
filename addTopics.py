259c1693b00179453632d2c63ced3858c409b499
lamonkey <88therisingsun@gmail.com>
2021-04-11 21:32:34 -0400

    #6 sovled by change version pyspark version to 2.4.6

--- addTopics.py
+++ addTopics.py
@@ -1,10 +1,18 @@
-from kafka import KafkaConsumer
+
 from kafka import KafkaProducer
 import time
+import csv
 producer = KafkaProducer(bootstrap_servers='localhost:9092')
-consumer = KafkaConsumer('tickets', group_id='tickets')
-for i in range(100):
-    print("added topic")
-    value = str(i)
-    producer.send('tickets', value = bytes(value,'utf-8'))
-    time.sleep(1)
+with open('data/ticket_flights.csv', newline='') as csvfile:
+    spamreader = csv.reader(csvfile)
+    for row in spamreader:
+        ticket = (', '.join(row))
+        producer.send('tickets', value = bytes(ticket,'utf-8'))
+        time.sleep(1)
+
+
+# for i in range(100):
+#     print("added topic")
+#     value = str(i)
+#     producer.send('tickets', value = bytes(value,'utf-8'))
+#     time.sleep(1)
