55ceebd3c2c8ad76b403babbc4e96308c6978da7
Hannah Han <46936828+HannahHan3@users.noreply.github.com>
2021-04-23 18:37:28 -0400

    change the abs path to env variables

--- tony/spark-up.sh
+++ tony/spark-up.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+### Please change the below path to your kafka path accordingly
+
 osascript -e 'tell app "Terminal"
     do script "kafka_2.13-2.7.0/bin/zookeeper-server-start.sh kafka_2.13-2.7.0/config/zookeeper.properties"
 end tell'
@@ -12,11 +14,17 @@ end tell'
 #     do script "python /Users/huiminhan/Documents/VT21Spring/CS5614BigDataEngineering/Project/Phase3/ticket_producer.py"
 # end tell'
 
-/usr/local/Cellar/apache-spark/2.4.7/sbin/start-master.sh
+#### Start spark master and workers
 
-/usr/local/Cellar/apache-spark/2.4.7/sbin/start-slave.sh spark://localhost:7077
+$SPARK_HOME/sbin/start-master.sh
 
-wc_producer = python $SPARK_HOME/wc_producer.py 
+$SPARK_HOME/sbin/start-slave.sh spark://localhost:7077
+
+# /usr/local/Cellar/apache-spark/2.4.7/sbin/start-master.sh
+
+# /usr/local/Cellar/apache-spark/2.4.7/sbin/start-slave.sh spark://localhost:7077
+
+# wc_producer = python $SPARK_HOME/wc_producer.py 
 
 # spark-submit --packages org.apache.spark:spark-streaming-kafka-0-8_2.11:2.4.7 kafka_wordcount.py zookeeper_server:2181 wordcounttopic
 
