dca7432d279d4328e8da56bb289e8176c84d6603
lamonkey <88therisingsun@gmail.com>
2021-04-29 10:36:44 -0400

    added boostrap for layout

--- streamData/views.py
+++ streamData/views.py
@@ -14,8 +14,8 @@ from .refractor_pipeline import PipelineRefractor
 class UploadFileForm(forms.Form):
     #title = forms.CharField(max_length=50)
     file = forms.FileField(
-        label='Select a file',
-        help_text='max. 42 megabytes'
+        label=''
+        ##help_text='max. 42 megabytes'
     )
 
 # Create your views here.
