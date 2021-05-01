a5ea27f9e9d982bc986b05df0593297b16206bcc
Hsieh Hsin Han <tony83126@gmail.com>
2021-04-30 20:37:50 -0400

    update ticket_producer.py w/ manual operation

--- kafka_streaming/ticket_producer.py
+++ kafka_streaming/ticket_producer.py
@@ -2,10 +2,15 @@ import kafka
 
 producer = kafka.KafkaProducer(bootstrap_servers=['localhost:9092'])
 
-with open('ticket_flights.csv', 'r') as f:
-    count = 0
-    for line in f:
-        producer.send('ticket_flights_stream', line.rstrip().encode())
-        count += 1
-    print(count, "records has been produced in 'ticket_flights_stream'")
-producer.flush()
+while True:
+    line = input("Enter data: ")
+    if line == "pushall":
+        with open('./data/ticket_flights.csv', 'r') as f:
+            count = 0
+            for line in f:
+                producer.send('ticket_flights_stream', line.rstrip().encode())
+                count += 1
+            print(count, "records has been produced in 'ticket_flights_stream'")
+        producer.flush()
+        continue
+    producer.send('ticket_flights_stream', line.rstrip().encode())
